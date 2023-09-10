80bbc9bf5067a749ff611d3ca386dcca3261b49d
desuchan27 <orenodesukouichi@gmail.com>
2023-09-10 16:14:26 +0800

    modified padding for name input

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -20,14 +20,14 @@ const Register = () => {
               </label>
             </div>
             <div className="flex items-center justify-between mt-2">
-              <div className="flex flex-col pr-2">
+              <div className="flex flex-col">
                 <input
                   type="text"
                   placeholder="First name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-              <div className="flex flex-col pr-2">
+              <div className="flex flex-col px-1">
                 <input
                   type="text"
                   placeholder="Middle name"
